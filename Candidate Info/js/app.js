e3dc2168e8a2cda5d7a6130137cfc4a506d8db70
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-03-17 13:01:54 -0500

    Add files via upload

---
+++ Candidate Info/js/app.js
@@ -0,0 +1,24 @@
+const information = {
+    "John Smith": {
+        name: `John Smith`,
+        salary: `$45,000`,
+        work_experience: `1 year of experience`,
+        personality: `Introvert`
+    },
+    "Mary Jane": {
+        name: `Mary Jane`,
+        salary: `$55,000`,
+        work_experience: `3 year of experience`,
+        personality: `Extrovert`
+    },
+    "Bob Ryan": {
+        name: `Bob Ryan`,
+        salary: `$85,000`,
+        work_experience: `25 year of experience`,
+        personality: `Introvert`
+    },
+}; 
+
+document.getElementById(`submitJohn`).addEventListener(`click`, function(){
+    console.log(1);
+});
